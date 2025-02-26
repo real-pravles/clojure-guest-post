abe1d7e48af124561aa0789caecd5e926c4da4d6
Pravles Redneckoff <me@pravles.com>
2025-02-26 18:15:04 +0200

    Initial commit.

---
+++ src/main/java/com/pravles/clojure_guest_post/ClojureGuestPostApplication.java
@@ -0,0 +1,13 @@
+package com.pravles.clojure_guest_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClojureGuestPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClojureGuestPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pravles/clojure_guest_post/ClojureGuestPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pravles.clojure_guest_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClojureGuestPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
